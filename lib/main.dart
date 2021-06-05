d3ab7bf1d04b5f82723a060fbbd4dd1d3812312a
Mohmed Anis Agwan <anishagwan@gmail.com>
2021-06-05 22:02:10 +0530

    Created the list of transactions container and edited a bit

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,47 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transactions.map(
                     (trx) => Card(
-                      child: Text(trx.title),
+                      child: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 15
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.purple, width: 2
+                              )
+                            ),
+                            child: Text(
+                              trx.amount.toString(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  trx.title,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                  trx.date.toString(),
+                                style: TextStyle(
+                                  color: Colors.grey
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
             )).toList(),
           )
         ],
